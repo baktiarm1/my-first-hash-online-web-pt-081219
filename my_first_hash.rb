4369fda14c35d12b91fa2e780c86e256b4bcfecd
Baktiar Miah <baktiar.m@gmail.com>
2019-08-23 23:09:52 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -11,9 +11,9 @@ def shipping_manifest
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
   
   the_manifest ={
-    'whale_bone_corsets': 5,
-    'porcelain_vase' : 2, 
-    'oil_painting' : 3
+    'whale_bone_corsets' => 5,
+    'porcelain_vase' => 2, 
+    'oil_painting' => 3
   }
 
 
